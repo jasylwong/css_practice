bd5e368d0aded2c3bc21f18cdd73f096e242f979
Jason <jasylwong@gmail.com>
2020-07-20 10:47:35 +0100

    allow deletion of tags

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -6,4 +6,11 @@ class TagsController < ApplicationController
   def show
     @tag = Tag.find(params[:id])
   end
+
+  def destroy
+    @tag = Tag.find(params[:id])
+    puts @tag
+    @tag.destroy
+    redirect_to tags_path
+  end
 end
